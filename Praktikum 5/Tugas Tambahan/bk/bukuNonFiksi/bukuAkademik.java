6bf4b127cfca42dd1f0d179338abc803957e4e36
anandabayu <160461302+anandabayu2611@users.noreply.github.com>
2024-03-24 16:32:44 +0700

    Delete Praktikum 5/Tugas Tambahan/bk directory

--- Praktikum 5/Tugas Tambahan/bk/bukuNonFiksi/bukuAkademik.java
+++
@@ -1,7 +0,0 @@
-package bukuNonFiksi;
-
-public class bukuAkademik extends bukuNonFiksi {
-    public bukuAkademik(String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga){
-        super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
-    }
-}
